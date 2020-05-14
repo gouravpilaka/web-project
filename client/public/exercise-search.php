d6e2849af8989e31d9d4cf76b80e3416bfb022ca
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-14 14:16:03 -0400

    get request updated

---
+++ client/public/exercise-search.php
@@ -0,0 +1,19 @@
+<?php
+require_once('conn.php');
+ 
+function get_exercise($conn , $term){ 
+ $query = "SELECT * FROM exercise WHERE exercise_name LIKE '%".$term."%' ORDER BY exercise_name ASC";
+ $result = mysqli_query($conn, $query); 
+ $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+ return $data; 
+}
+ 
+if (isset($_GET['term'])) {
+ $getexercise = get_exercise($conn, $_GET['term']);
+ $exerciseList = array();
+ foreach($getexercise as $exercise){
+ $exerciseList[] = $exercise['exercise_name'];
+ }
+ echo json_encode($exerciseList);
+}
+?>
